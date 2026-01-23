b9f8f78e045341539437cdb47fb4535dfd793184
sergioneto-tech <tech@sergioneto.pt>
2026-01-23 11:00:25 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
+import { AppStoreProvider } from "./lib/AppStore";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppStoreProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppStoreProvider>
+  </React.StrictMode>
+);

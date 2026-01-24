b3e4f6f12b34704fdec22026bd5ecfbf72531dcb
sergioneto-tech <tech@sergioneto.pt>
2026-01-24 16:05:35 +0000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 import { AppStoreProvider } from "./lib/AppStore";
-import App from "./App";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { AuthProvider, useAuth } from "./lib/auth";
+
+function StoreWithAuthKey({ children }: { children: React.ReactNode }) {
+  const { storageKey } = useAuth();
+  return (
+    <AppStoreProvider storageKey={storageKey} key={storageKey}>
+      {children}
+    </AppStoreProvider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <AppStoreProvider>
-        <App />
-      </AppStoreProvider>
-    </HashRouter>
+    <AuthProvider>
+      <HashRouter>
+        <StoreWithAuthKey>
+          <TooltipProvider>
+            <App />
+          </TooltipProvider>
+          <Toaster />
+        </StoreWithAuthKey>
+      </HashRouter>
+    </AuthProvider>
   </React.StrictMode>
 );

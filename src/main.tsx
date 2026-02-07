897d85973adb27cde9203e4818351b1d0388efd8
sergioneto-tech <tech@sergioneto.pt>
2026-02-07 17:44:18 +0000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
+import { applyTheme, getStoredTheme, getSystemTheme } from "@/lib/theme";
+import { UpdateProvider } from "@/lib/UpdateProvider";
 import { AppStoreProvider } from "./lib/AppStore";
 import App from "./App";
+
+// Aplicar tema o mais cedo possível (evita "flash" ao abrir)
+const initialTheme = getStoredTheme() ?? getSystemTheme();
+applyTheme(initialTheme);
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <AppStoreProvider>
-        <App />
-      </AppStoreProvider>
+      <UpdateProvider>
+        <AppStoreProvider>
+          <App />
+        </AppStoreProvider>
+      </UpdateProvider>
     </HashRouter>
   </React.StrictMode>
 );

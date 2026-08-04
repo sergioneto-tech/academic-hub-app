f55b37530a0c3ef6e55c87c1318cc63cf4eb03c9
sergioneto-tech <tech@sergioneto.pt>
2026-08-04 18:14:18 +0100

    Add legal and academic report routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import ResetPasswordPage from "./pages/ResetPassword";
 import StudyPlan from "./pages/StudyPlan";
 import PersonalStudyPlan from "./pages/PersonalStudyPlan";
 import HelpPage from "./pages/Help";
+import LegalPage from "./pages/Legal";
+import AcademicReportPage from "./pages/AcademicReport";
 import { useAutoSync } from "./hooks/useAutoSync";
 
 export default function App() {
@@ -24,11 +26,13 @@ export default function App() {
         <Route path="/cadeiras/:id" element={<CourseDetailPremium />} />
         <Route path="/calendario" element={<CalendarPage />} />
         <Route path="/historico" element={<HistoryPage />} />
+        <Route path="/historico/relatorio" element={<AcademicReportPage />} />
         <Route path="/plano" element={<StudyPlan />} />
         <Route path="/plano/estudo" element={<PersonalStudyPlan />} />
         <Route path="/definicoes" element={<SettingsPremium />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
         <Route path="/ajuda" element={<HelpPage />} />
+        <Route path="/legal" element={<LegalPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>

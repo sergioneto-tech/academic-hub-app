23aec0b362fb4d5da4be3af5fb06d223c5acb7d4
sergioneto-tech <tech@sergioneto.pt>
2026-09-16 17:10:42 +0100

    release: refresh service worker for 1.5.10

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "1.5.9";
-const SW_VERSION = "1.5.9-controlled-update-1";
+const APP_VERSION = "1.5.10";
+const SW_VERSION = "1.5.10-auth-confirmation-1";
 const CACHE = `academic-hub-${SW_VERSION}`;
 const APP_SHELL_KEY = new URL("./__academic_hub_app_shell__", self.location.href).href;
 const NOTIFICATION_ICON = "./academic-hub-notification-gold.svg";
@@ -15,7 +15,7 @@ const PRECACHE_URLS = [
   "./academic-hub-icon-v10-512.png",
   NOTIFICATION_ICON,
   NOTIFICATION_BADGE,
-  "./release-notes.json?v=1.5.9",
+  "./release-notes.json?v=1.5.10",
 ];
 
 function delay(ms) {
